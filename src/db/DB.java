1db879ae2218970b6083dfe1c9eadbd2dc450c95
WillianJrBastos <wjbastos1@gmail.com>
2026-04-29 09:59:00 -0300

    Adicionada a conexão com o banco de dados

--- src/db/DB.java
+++ src/db/DB.java
@@ -1,9 +1,31 @@
 package db;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class DB {
+
+    public static Connection conn = null;
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                Properties props = loadProperties();
+
+                String url = props.getProperty("dburl");
+
+                conn = DriverManager.getConnection(url, props);
+
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+
+        return conn;
+    }
 
     public static Properties loadProperties() {
 
